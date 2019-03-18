1ab02988b59d368e498e664efdc37debbf8a224f
James W. Lane <james.w.lane@mac.com>
2019-03-17 22:28:09 -0500

    build(eslint): Added ESLint

--- jest.config.js
+++ jest.config.js
@@ -1,18 +1,12 @@
 module.exports = {
-  globals: {
-    'ts-jest': {
-      isolatedModules: true,
-      tsConfig: 'tsconfig.json',
-    },
-  },
-  testEnvironment: 'node',
-  testMatch: [
-    '<rootDir>/test/**/*.(test|spec).(ts|tsx|js)',
-  ],
+  clearMocks: true,
   coveragePathIgnorePatterns: [
     '<rootDir>/test/helpers/',
     '<rootDir>/node_modules/',
   ],
-  clearMocks: true,
   preset: 'ts-jest',
-}
+  testEnvironment: 'node',
+  testMatch: [
+    '<rootDir>/test/**/*.(test|spec).(ts|tsx|js)',
+  ],
+};
